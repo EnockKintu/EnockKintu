6c21f71197d156b6c5ed93d775f50d6d99bdf6d3
EnockKintu <94127559+EnockKintu@users.noreply.github.com>
2021-11-25 15:10:00 +0000

    Update SOB 28.py

--- SOB 28.py
+++ SOB 28.py
@@ -1,8 +1,10 @@
+#The first half of the asterix pattern
 n = 5
 for i in range(n):
     for j in range(i+1):
         print('*', end=' ')
     print()
+#The second half of the asterix pattern
 n = 4
 for i in range(n):
     for j in range(i,n):
