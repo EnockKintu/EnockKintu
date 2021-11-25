78e85da9d1458690557cb444d0a1558789b9faa5
EnockKintu <94127559+EnockKintu@users.noreply.github.com>
2021-11-25 15:02:19 +0000

    Python asterix pattern

---
+++ Sob 29.py
@@ -0,0 +1,8 @@
+for row in range(9):
+    for col in range(5):
+        if col-row==0 or row+col==8:
+            print("*", end="")
+        else:
+            print(end=" ")
+    print()
+          
